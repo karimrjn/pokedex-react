f50129980c3dd3887e716167ce26aaff65963eb0
Karim Rejani <karim.rjn02@gmail.com>
2023-10-10 12:27:07 +0200

    finished exercise from React quest 4 props Pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const bulbasaur = pokemonList.filter(pokemon => pokemon.name === 'bulbasaur');
+
   return (
     <div>
-      <PokemonCard />
+      {bulbasaur.map((pokemon, index) => (
+        <PokemonCard key={index} pokemon={pokemon} />
+      ))}
     </div>
   );
 }
